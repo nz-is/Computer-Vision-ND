c21575a785a795c8406a7ca02a88cf456c95624f
blkpnk182 <naz.182@hotmail.com>
2019-11-21 23:15:27 +0000

    refactoring code

--- P1_Facial_Keypoints/models.py
+++ P1_Facial_Keypoints/models.py
@@ -1,5 +1,3 @@
-## TODO: define the convolutional neural network architecture
-
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
@@ -7,19 +5,14 @@ import torch.nn.functional as F
 import logging
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
+from torchvision import models
+from collections import OrderedDict
 
 #Implementation of NaimishNet
 class NaimishNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channel=1, out_channel=[32, 64, 128, 256],
+                 dropout_prob=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6]):
         super(NaimishNet, self).__init__()
-
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature map s, 5x5 square convolution kernel
 
         #Architecture summary
         #Activation Layer 1 to 5 is ELU
@@ -32,95 +25,113 @@ class NaimishNet(nn.Module):
         #OUT conv1: ( 32, 221, 221)
         #maxpool1: (32, 110, 110)
         #Layer 1 out: (32, 110, 110)
-        self.conv1 = nn.Conv2d(1, 32, 4)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.dropout1 = nn.Dropout2d(0.1)
+        self.conv1 = nn.Sequential(
+            OrderedDict([
+            ('conv1',  nn.Conv2d(in_channel, out_channel[0], 4)),
+            ('elu_1', nn.ELU()),
+            ('bn1', nn.BatchNorm2d(out_channel[0])),
+            ('dropout_1', nn.Dropout2d(dropout_prob[0])) ]
+            )
+        )
 
         #LAYER 2
         #IN ( 32, 110, 110)
         #conv2 ( 64, 108, 108)
         #maxpool2: (64, 53, 53)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.bn2 = nn.BatchNorm2d(64)
-
-        self.dropout2 = nn.Dropout2d(0.2)
+        self.conv2 = nn.Sequential(
+            OrderedDict([
+            ('conv2',  nn.Conv2d(out_channel[0], out_channel[1], 3)),
+            ('elu_2', nn.ELU()),
+            ('bn2', nn.BatchNorm2d(out_channel[1])),
+            ('dropout_2', nn.Dropout2d(dropout_prob[1])) ]
+            )
+        )
 
         #LAYER 3
         #IN (64, 53, 53)
         #Conv3: (128, 52, 52)
         #maxpool: (128, 26, 26)
-        self.conv3 = nn.Conv2d(64, 128, 2)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.dropout3 = nn.Dropout2d(0.3)
+        self.conv3 = nn.Sequential(
+            OrderedDict([
+            ('conv3',  nn.Conv2d(out_channel[1], out_channel[2], 2)),
+            ('elu_3', nn.ELU()),
+            ('bn3', nn.BatchNorm2d(out_channel[2])),
+            ('dropout_3', nn.Dropout2d(dropout_prob[2])) ]
+            )
+        )
 
         #Layer 4
         #IN (128, 26, 26)
         #conv4: (256, 26, 26)
         #maxpool4: ( 256, 13, 13)
-        self.conv4 = nn.Conv2d(128, 256, 1)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.dropout4 = nn.Dropout2d(0.4)
+        self.conv4 = nn.Sequential(
+            OrderedDict([
+            ('conv4',  nn.Conv2d(out_channel[2], out_channel[3], 1)),
+            ('elu_4', nn.ELU()),
+            ('bn4' , nn.BatchNorm2d(out_channel[3])),
+            ('dropout_4', nn.Dropout2d(dropout_prob[3])) ]
+            )
+        )
 
         #IN( 256, 5, 5)
         #Flatten (256 * 13* 13)
-        self.fc1 = nn.Linear(in_features=13*13*256, out_features=1000)
-        self.bn5 = nn.BatchNorm1d(1000)
-        self.dropout5 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Sequential(
+            OrderedDict([
+                         ('fc1', nn.Linear(in_features=13*13*256, out_features=1000)),
+                          ('elu_5' , nn.ELU()),
+                          ('bn5' , nn.BatchNorm1d(1000)),
+                         ('dropout_5' , nn.Dropout2d(dropout_prob[4]))
+            ])
+        )
 
-        #Layer 6
-        self.fc2 = nn.Linear(in_features=1000, out_features=500)
-        self.bn6 = nn.BatchNorm1d(500)
-        self.dropout6 = nn.Dropout2d(0.6)
+        self.fc2 = nn.Sequential(
+            OrderedDict([
+                         ('fc2' , nn.Linear(in_features=1000, out_features=500)),
+                          ('tanh_6' , nn.Tanh()),
+                          ('bn6' , nn.BatchNorm1d(500)),
+                         ('dropout_6' , nn.Dropout2d(dropout_prob[5]))
+            ])
+        )
 
         #Layer 7
         #OUT FKP: (X, Y)
         self.fc3 = nn.Linear(in_features=500, out_features=136)
 
         #Custom weights initialization
-
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 m.weight = I.uniform_(m.weight, a = 0.0, b = 1.0)
             elif isinstance(m, nn.Linear):
                 m.weight= I.xavier_uniform_(m.weight, gain=1)
 
-
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-
         #LAYER 1
         #IN (1, 224, 224)
         #OUT conv1: ( 32, 221, 221)
         #maxpool1: ( 32, 110, 110)
         #Layer 1 out: ( 32, 110, 110)
-        x = self.pool(F.elu(self.bn1(self.conv1(x))))
-        x = self.dropout1(x)
+        x = self.pool(self.conv1(x))
         #print(f"CONV1: {x.size()}")
 
         #LAYER 2
         #IN ( 32, 110, 110)
         #conv2 ( 64, 44, 44)
         #maxpool2: (64, 22, 22)
-        x = self.pool(F.elu(self.bn2(self.conv2(x))))
-        x = self.dropout2(x)
+        x = self.pool(self.conv2(x))
         #print(f"CONV2: {x.size()}")
 
         #LAYER 3
         #IN (64, 22, 22)
         #Conv3: (128, 21, 21)
         #maxpool: (128, 10, 10)
-        x = self.pool(F.elu(self.bn3(self.conv3(x))))
-        x = self.dropout3(x)
+        x = self.pool(self.conv3(x))
         #print(f"CONV3: {x.size()}")
 
         #Layer 4
         #IN (128, 10, 10)
         #conv4: (256, 10, 10)
         #maxpool4: ( 256, 5, 5)
-        x = self.pool(F.elu(self.bn4(self.conv4(x))))
-        x = self.dropout4(x)
+        x = self.pool(self.conv4(x))
 
         #Layer 5
         #print(f"CONV4: {x.size()}")
@@ -131,18 +142,28 @@ class NaimishNet(nn.Module):
         #IN: ( 256, 5, 5)
         #Flatten (256 * 5 * 5) = 6400
         #OUT: (1000)
-        x = F.elu(self.bn5(self.fc1(x)))
-        x = self.dropout5(x)
+        x = self.fc1(x)
 
         #Layer 6
         #IN: (1000)
         #out: (500)
-        x = torch.tanh(self.bn6(self.fc2(x)))
-        x = self.dropout6(x)
+        x = self.fc2(x)
 
         #Final Layer 7
         #OUT FKP: (X, Y)
         x = self.fc3(x)
 
         # a modified x, having gone through all the layers of your model, should be returned
+        return x
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(ResNet18, self).__init__()
+        self.resnet18 = models.resnet18(pretrained=True)
+        self.resnet18.conv1 = nn.Conv2d(1, 64, kernel_size=(7,7), stride=(2, 2), padding=(3,3), bias=False)
+        n_inputs = self.resnet18.fc.in_features
+        self.resnet18.fc = nn.Linear(n_inputs, 136)
+
+    def forward(self, x):
+        x = self.resnet18(x)
         return x
